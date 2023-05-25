b2a693cde5da3ff1d58273f11eeb3148f67aee25
Tony Mullen <tonymullen@Tonys-MacBook-Pro.local>
2023-05-25 11:46:15 -0700

    Fixes

---
+++ server.js
@@ -0,0 +1,69 @@
+const http = require('http');
+const fs = require('fs');
+const HTML_DIR = './build';
+const PORT = process.env.PORT || 3000;
+const OUTPUT_FILE= './output/30_022_128_1_0_Questions_annot.json';
+
+const server = http.createServer(onRequest);
+
+server.listen(PORT);
+console.log("Starting server.js...")
+
+
+function onRequest(request, response) {
+    if (request.method == 'POST') {
+        console.log(request.url);
+        var body = "";
+        request.on('readable', function() {
+            console.log("Reading...");
+            r = request.read();
+            if (r != null) {
+                body += r;
+            }
+            // body += request.read();
+        });
+        request.on('end', function() {
+            console.log("End of request.");
+            console.log(body);
+            fs.writeFile(`${OUTPUT_FILE}`, JSON.stringify(JSON.parse(body).data), err => {
+                if (err) {
+                  console.error(err);
+                }
+                // file written successfully
+              });
+            response.writeHead(200);
+            response.end();
+        });
+    } else {
+        if ('/' == request.url) {
+            console.log("Redirecting to index.html...")
+            request.url = '/index.html';
+        }
+        if ('/annotate' == request.url) { 
+            console.log("Reading output file...")
+            fs.readFile(OUTPUT_FILE, function(err, data) {
+                if (err) {
+                    console.log(err);
+                    response.writeHead(404);
+                    response.end(JSON.stringify(err));
+                    return;
+                }
+                console.log("Sending output file...")
+                response.writeHead(200);
+                response.end(data);
+                return;
+            });
+        } else {
+            fs.readFile(HTML_DIR + request.url, function(err, data) {
+                console.log(HTML_DIR + request.url)
+                if (err) {
+                    response.writeHead(404);
+                    response.end(JSON.stringify(err));
+                    return;
+                }
+                response.writeHead(200);
+                response.end(data);
+            });
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import raw_data from './data/30_022_128_1_0_Questions.json';
+
+
+const URL = 'http://10.0.0.100:3000';
+
 
 function App() {
+
+  const [outputData, setOutputData] = useState([]);
+  const [dataIndex, setDataIndex] = useState(0);
+
+  useEffect(() => {
+    console.log("Getting data...")
+    console.log(`URL: ${URL}/annotate`)
+    axios.get(`${URL}/annotate`).then(res => {
+      console.log("Got data!");
+      console.log(res.data);
+      setOutputData(res.data);
+      setDataIndex(res.data.length);
+      console.log("Res data length: " + res.data.length);
+    });
+  }, []);
+
+  function annotate(val) {
+    console.log(dataIndex, val);
+    setOutputData([...outputData, {'index': dataIndex, 'label': val}]);
+    console.log(outputData);
+    axios.post(`${URL}/annotate`, {'data': outputData}).then(res => {
+      if (res.status == 200) {
+        console.log("Posted data successfully");
+        setDataIndex(dataIndex + 1);
+      } else {
+        alert("Error posting data, please open a new tab and try again.")
+      }
+    });
+  }
+
+  function format(rawstr) {
+    let newlines = rawstr.replaceAll(/\n/msg, '<br>');
+    let boldlines = newlines.replaceAll(/(NATIVE .*? SPEAKER:)/msg, '<strong>$1</strong><br>');
+    return boldlines;
+  }
+
+  console.log("Data index " + dataIndex)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p 
+          style={{backgroundColor: 'white', color: 'black', padding: 20}}
+          dangerouslySetInnerHTML={{__html: format(
+            // 'NATIVE SPANISH SPEAKER:\t&=laughs &-ay my god.\nNATIVE ENGLISH SPEAKER:\txxx no idea xxx &=laughs here we go.\n'
+              raw_data[dataIndex]['dialog']
+          )}} />
+        <div>
+          <Button 
+            style={{margin:10}}
+            onClick={() => annotate('warm')}
+            variant="warning">Warm</Button>{' '}
+          <Button 
+            style={{margin:10}}
+            onClick={() => annotate('cool')}
+            variant="info">Cool</Button>{' '}
+        </div>
+        <div>
+          <Button 
+            style={{margin:10}}
+            onClick={() => annotate('neutral')}
+            variant="secondary">Neutral</Button>{' '}
+        </div>
       </header>
     </div>
   );
